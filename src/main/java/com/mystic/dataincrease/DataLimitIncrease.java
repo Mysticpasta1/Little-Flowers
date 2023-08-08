a92a481224dcce040ba2e51bb967c10e6104fafb
Mysticpasta1 <ethana5tc@outlook.com>
2023-08-08 16:04:22 -0500

    start

--- src/main/java/com/mystic/dataincrease/DataLimitIncrease.java
+++ src/main/java/com/mystic/dataincrease/DataLimitIncrease.java
@@ -1,14 +1,13 @@
-package com.yourname.modid;
+package com.mystic.dataincrease;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Example.MOD_ID)
-public class Example {
-    public static final String MOD_ID = "example";
+@Mod(modid = DataLimitIncrease.MOD_ID)
+public class DataLimitIncrease {
+    public static final String MOD_ID = "dataincrease";
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
-        System.out.println("Hello world!");
     }
 }
